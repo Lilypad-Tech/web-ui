66482f207c490a7d0e0c3009e5b9ff4146b14803
Nadiem <nadiem8@msn.com>
2024-05-30 16:49:13 +0200

    Add TableLeadText component

---
+++ apps/info-dashboard/src/components/Table/TableLeadText.tsx
@@ -0,0 +1,32 @@
+interface TableLeadTextChildren {
+  title?: React.ReactNode;
+  subtitle?: React.ReactNode;
+}
+
+interface TableLeadTextProps
+  extends Omit<React.HTMLProps<HTMLDivElement>, "children"> {
+  children?: TableLeadTextChildren;
+}
+
+const TableLeadText = ({ children, ...props }: TableLeadTextProps) => {
+  const { className = "", ...rest } = props;
+  return (
+    <div
+      className={`${className} px-uui-3xl py-uui-xl flex items-center justify-start`}
+      {...rest}
+    >
+      {children?.title ? (
+        <span className="text-uui-text-sm antialiased text-uui-text-primary-900 font-medium text-left ">
+          {children.title}
+        </span>
+      ) : null}
+      {children?.subtitle ? (
+        <span className="text-uui-text-sm antialiased text-uui-text-tertiary-600 font-normal text-left">
+          {children?.subtitle}
+        </span>
+      ) : null}
+    </div>
+  );
+};
+
+export default TableLeadText;
