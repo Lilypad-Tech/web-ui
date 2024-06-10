2936b8714d06efca57d1f747474f32284a0e8351
Nadiem <nadiem8@msn.com>
2024-06-10 18:01:41 +0200

    feat: add BadgeTextAtom to BadgeComponent

---
+++ apps/info-dashboard/src/components/Badge/BadgeTextAtom.tsx
@@ -0,0 +1,17 @@
+import { BadgeSize } from "./BadgeTypes";
+
+type BadgeTextAtomProps = {
+  children: React.ReactNode;
+  size: BadgeSize;
+};
+
+const BadgeTextAtom = ({ children, size }: BadgeTextAtomProps) => {
+  const layer1 = "antialiased font-medium";
+  const sizes = { sm: "uui-text-xs" };
+
+  const classes = `${layer1} ${sizes[size]}`;
+
+  return <span className={`${classes}`}>{children}</span>;
+};
+
+export default BadgeTextAtom;
