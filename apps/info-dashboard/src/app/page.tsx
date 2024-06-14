a97fd9e7247676d375ffafc71ab833467f42194b
Benjamin Preiss <benjaminpreiss@protonmail.com>
2024-06-14 15:52:24 +0200

    refactor: Adjust font styles and responsive behaviour on metrics page

--- apps/info-dashboard/src/app/page.tsx
+++ apps/info-dashboard/src/app/page.tsx
@@ -9,94 +9,105 @@ import SectionContainer from "@/components/SectionContainer";
 import { arrowsArrowDown, arrowsArrowUp } from "@frontline-hq/untitledui-icons";
 
 const metricsData = [
-  {
-    title: m.metrics_card_1_title(),
-    header: "2.000",
-    percentage: "100%",
-  },
-  {
-    title: m.metrics_card_2_title(),
-    header: "2.000",
-    percentage: "0%",
-  },
-  {
-    title: m.metrics_card_3_title(),
-    header: "2.000",
-    percentage: "-100%",
-  },
+	{
+		title: m.metrics_card_1_title(),
+		header: "2.000",
+		percentage: "100%",
+	},
+	{
+		title: m.metrics_card_2_title(),
+		header: "2.000",
+		percentage: "0%",
+	},
+	{
+		title: m.metrics_card_3_title(),
+		header: "2.000",
+		percentage: "-100%",
+	},
 ];
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Lilypad Metrics Dashboard - Monitor the Lilypad Network!</title>
-        <meta
-          name="description"
-          content="Explore the metrics dashboard for the Lilypad network. Gain insights into the decentralized world of compute."
-        />
-        <meta name="robots" content="index, follow" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="canonical" href="/" />
-        <meta
-          property="og:title"
-          content="Node Provider Metrics Dashboard - Monitor the Lilypad Network!"
-        />
-        <meta
-          property="og:description"
-          content="Explore the metrics dashboard for the Lilypad network. Gain insights into the decentralized world of compute."
-        />
-        <meta property="og:url" content="/" />
-        <meta property="og:type" content="website" />
-      </Head>
-      {/* <Helmet encodeSpecialCharacters={true}>
+	return (
+		<>
+			<Head>
+				<title>
+					Lilypad Metrics Dashboard - Monitor the Lilypad Network!
+				</title>
+				<meta
+					name="description"
+					content="Explore the metrics dashboard for the Lilypad network. Gain insights into the decentralized world of compute."
+				/>
+				<meta name="robots" content="index, follow" />
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1"
+				/>
+				<link rel="canonical" href="/" />
+				<meta
+					property="og:title"
+					content="Node Provider Metrics Dashboard - Monitor the Lilypad Network!"
+				/>
+				<meta
+					property="og:description"
+					content="Explore the metrics dashboard for the Lilypad network. Gain insights into the decentralized world of compute."
+				/>
+				<meta property="og:url" content="/" />
+				<meta property="og:type" content="website" />
+			</Head>
+			{/* <Helmet encodeSpecialCharacters={true}>
 				<script type="application/ld+json">
 					{JSON.stringify(jsonLd)}
 				</script>
 			</Helmet> */}
-      <div className=" ">
-        <HeadingSection
-          className="pt-uui-6xl"
-          title={m.metrics_heading_title()}
-          subtitle={m.metrics_heading_subtitle()}
-        />
-        <SectionContainer className="sm:pt-uui-container-padding-desktop mx-auto pt-uui-container-padding-mobile w-full justify-between flex space-x-uui-3xl">
-          {metricsData.map((data, index) => {
-            const percentage = Number(data.percentage.replace("%", ""));
+			<div className=" ">
+				<HeadingSection
+					className="pt-uui-4xl uui-desktop:pt-uui-6xl"
+					title={m.metrics_heading_title()}
+					subtitle={m.metrics_heading_subtitle()}
+				/>
+				<SectionContainer className=" mx-auto pt-uui-4xl w-full justify-between flex uui-desktop:gap-uui-3xl gap-uui-2xl sm:flex-row flex-col  snap-x overflow-x-auto no-scrollbar">
+					{metricsData.map((data, index) => {
+						const percentage = Number(
+							data.percentage.replace("%", "")
+						);
 
-            return (
-              <MetricsCard key={index} title={data.title} header={data.header}>
-                {{
-                  badge: (
-                    <Badge
-                      icon={{
-                        type: "icon",
-                        leading:
-                          percentage > 0
-                            ? arrowsArrowUp
-                            : percentage === 0
-                            ? undefined
-                            : arrowsArrowDown,
-                      }}
-                      badgeType="Pill color"
-                      color={
-                        percentage > 0
-                          ? "success"
-                          : percentage === 0
-                          ? "warning"
-                          : "error"
-                      }
-                      size="md"
-                    >
-                      {data.percentage}
-                    </Badge>
-                  ),
-                }}
-              </MetricsCard>
-            );
-          })}
-        </SectionContainer>
-      </div>
-    </>
-  );
+						return (
+							<MetricsCard
+								key={index}
+								title={data.title}
+								header={data.header}
+							>
+								{{
+									badge: (
+										<Badge
+											icon={{
+												type: "icon",
+												leading:
+													percentage > 0
+														? arrowsArrowUp
+														: percentage === 0
+														? undefined
+														: arrowsArrowDown,
+											}}
+											badgeType="Pill color"
+											color={
+												percentage > 0
+													? "success"
+													: percentage === 0
+													? "warning"
+													: "error"
+											}
+											size="md"
+										>
+											{data.percentage}
+										</Badge>
+									),
+								}}
+							</MetricsCard>
+						);
+					})}
+				</SectionContainer>
+			</div>
+		</>
+	);
 }
--- apps/info-dashboard/src/components/MetricsCard/MetricsCard.tsx
+++ apps/info-dashboard/src/components/MetricsCard/MetricsCard.tsx
@@ -1,40 +1,40 @@
 interface MetricsCardProps
-  extends Omit<React.HTMLAttributes<HTMLDivElement>, "children"> {
-  title: string;
-  header: string;
-  children?: MetricsCardChildrenProps;
+	extends Omit<React.HTMLAttributes<HTMLDivElement>, "children"> {
+	title: string;
+	header: string;
+	children?: MetricsCardChildrenProps;
 }
 
 interface MetricsCardChildrenProps {
-  badge?: React.ReactNode;
+	badge?: React.ReactNode;
 }
 
 const MetricsCard: React.FC<MetricsCardProps> = ({
-  title,
-  header,
-  children,
-  ...props
+	title,
+	header,
+	children,
+	...props
 }) => {
-  return (
-    <div
-      {...props}
-      className="w-full h-fit rounded-uui-xl border-uui-1 border-uui-border-secondary flex flex-col justify-between"
-    >
-      <div className="p-uui-3xl flex flex-col space-y-uui-md">
-        <span className=" text-uui-text-tertiary-600 font-uui-text-sm text-uui-text-sm font-medium">
-          {title}
-        </span>
-        <div className="flex items-center justify-between space-x-uui-xl">
-          <div>
-            <h2 className="text-uui-display-md font-uui-display-md text-uui-text-primary-900 font-semibold">
-              {header}
-            </h2>
-          </div>
-          {children?.badge}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			{...props}
+			className="w-full snap-center h-fit rounded-uui-xl border-uui-1 border-uui-border-secondary flex flex-col justify-between bg-uui-bg-primary_alt"
+		>
+			<div className="p-uui-3xl flex flex-col space-y-uui-md">
+				<span className=" text-uui-text-tertiary-600 uui-text-sm font-medium">
+					{title}
+				</span>
+				<div className="flex items-end justify-between space-x-uui-xl">
+					<div>
+						<h2 className=" uui-display-md text-uui-text-primary-900 font-semibold">
+							{header}
+						</h2>
+					</div>
+					<div className="pb-uui-md">{children?.badge}</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default MetricsCard;
