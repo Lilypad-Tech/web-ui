ed8214d2470f8161ab246ab88c488a28f5336973
Nadiem <nadiem8@msn.com>
2024-06-24 17:47:46 +0200

    feat:add-badge-component-to-status-column

--- apps/info-dashboard/src/lib/fetchers/leaderboard.ts
+++ apps/info-dashboard/src/lib/fetchers/leaderboard.ts
@@ -1,26 +1,27 @@
 import { ascending, sort } from "d3";
 export type LeaderboardReturnType = {
-	Points: string;
-	Rank: string;
-	Energy: string;
-	Wallet: string;
+  Points: string;
+  Rank: string;
+  Energy: string;
+  Wallet: string;
 }[];
 
 export async function fetchLeaderboard() {
-	const api_host = process.env.NEXT_PUBLIC_API_HOST;
-	const leaderboard_url = `${api_host}metrics-dashboard/leaderboard`;
-	const raw = await fetch(leaderboard_url);
-	return (await raw.json()) as LeaderboardReturnType;
+  const api_host = process.env.NEXT_PUBLIC_API_HOST;
+  const leaderboard_url = `${api_host}metrics-dashboard/leaderboard`;
+  const raw = await fetch(leaderboard_url);
+  return (await raw.json()) as LeaderboardReturnType;
 }
 
 export function toTableData(data: LeaderboardReturnType) {
-	return sort(data, (a, b) => ascending(a.Rank, b.Rank)).map(
-		({ Rank, Wallet, Energy, Points }) => ({
-			Rank,
-			Wallet,
-			"Energy Provided": Energy,
-			"Reward Points": Points,
-			Share: "share",
-		})
-	);
+  return sort(data, (a, b) => ascending(a.Rank, b.Rank)).map(
+    ({ Rank, Wallet, Energy, Points }) => ({
+      Rank,
+      Wallet,
+      "Energy Provided": Energy,
+      "Reward Points": Points,
+      Status: Math.random() > 0.5 ? true : false,
+      Share: "share",
+    })
+  );
 }
