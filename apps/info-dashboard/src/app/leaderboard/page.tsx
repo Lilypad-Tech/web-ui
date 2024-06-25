b5462d53b7db78e9bc8bccb33ae95c540cc399b1
Nadiem <nadiem8@msn.com>
2024-06-25 13:19:32 +0200

    feat:update-lilypad-tweet-message

--- apps/info-dashboard/src/app/leaderboard/page.tsx
+++ apps/info-dashboard/src/app/leaderboard/page.tsx
@@ -4,10 +4,10 @@ import { useQuery } from "@tanstack/react-query";
 import Table from "@/components/Table/Table";
 import FeaturedIcon from "@/components/FeaturedIcon";
 import {
-	generalLoading01,
-	alertAndFeedbackAlertCircle,
-	generalSearchLg,
-	generalSearchMd,
+  generalLoading01,
+  alertAndFeedbackAlertCircle,
+  generalSearchLg,
+  generalSearchMd,
 } from "@frontline-hq/untitledui-icons";
 import CardHeader from "@/components/CardHeader";
 import InputField from "@/components/InputField/Inputfield";
@@ -19,9 +19,9 @@ import SocialIcon from "@/components/SocialIcon";
 import * as m from "@/paraglide/messages.js";
 import Head from "next/head";
 import {
-	fetchLeaderboard,
-	getHeaderData,
-	toTableData,
+  fetchLeaderboard,
+  getHeaderData,
+  toTableData,
 } from "@/lib/fetchers/leaderboard";
 import Badge from "@/components/Badge/Badge";
 import EmptyState from "@/components/EmptyState/EmptyState";
@@ -30,300 +30,226 @@ import Tooltip from "@/components/Tooltip/Tooltip";
 // `${API_HOST}metrics-dashboard/metrics` is the endpoint for the metrics dashboard
 
 export default function Leaderboard() {
-	const [walletAddress, setWalletAddress] = useState("");
-	const { data, isLoading, isError } = useQuery({
-		queryFn: fetchLeaderboard,
-		queryKey: ["leaderboard"], //Array according to Documentation
-	});
+  const [walletAddress, setWalletAddress] = useState("");
+  const { data, isLoading, isError } = useQuery({
+    queryFn: fetchLeaderboard,
+    queryKey: ["leaderboard"], //Array according to Documentation
+  });
 
-	const [currentUrl, setCurrentUrl] = useState("");
+  const [currentUrl, setCurrentUrl] = useState("");
 
-	useEffect(() => {
-		if (typeof window !== "undefined") {
-			setCurrentUrl(
-				encodeURIComponent(
-					window.location.origin + window.location.pathname
-				)
-			);
-			let searchParams = new URLSearchParams(window.location.search);
-			const walletId = searchParams.get("wallet_id");
-			if (walletId) setWalletAddress(walletId);
-		}
-	}, []);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setCurrentUrl(
+        encodeURIComponent(window.location.origin + window.location.pathname)
+      );
+      let searchParams = new URLSearchParams(window.location.search);
+      const walletId = searchParams.get("wallet_id");
+      if (walletId) setWalletAddress(walletId);
+    }
+  }, []);
 
-	return (
-		<>
-			<Head>
-				<title>
-					Lilypad Leaderboard - Become the Top Node Provider!
-				</title>
-				<meta
-					name="description"
-					content="Check out the leaderboard for decentralized compute network node providers. Compare your rank, see your reward points, and aim to be the best!"
-				/>
-				<meta name="robots" content="index, follow" />
-				<meta
-					name="viewport"
-					content="width=device-width, initial-scale=1"
-				/>
-				<link rel="canonical" href="/" />
-				<meta
-					property="og:title"
-					content="Node Provider Leaderboard - Become the Top Node Provider!"
-				/>
-				<meta
-					property="og:description"
-					content="Check out the leaderboard for decentralized compute network node providers. Compare your rank, see your reward points, and aim to be the best!"
-				/>
-				<meta property="og:url" content="/" />
-				<meta property="og:type" content="website" />
-			</Head>
-			{/* <Helmet encodeSpecialCharacters={true}>
+  return (
+    <>
+      <Head>
+        <title>Lilypad Leaderboard - Become the Top Node Provider!</title>
+        <meta
+          name="description"
+          content="Check out the leaderboard for decentralized compute network node providers. Compare your rank, see your reward points, and aim to be the best!"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="canonical" href="/" />
+        <meta
+          property="og:title"
+          content="Node Provider Leaderboard - Become the Top Node Provider!"
+        />
+        <meta
+          property="og:description"
+          content="Check out the leaderboard for decentralized compute network node providers. Compare your rank, see your reward points, and aim to be the best!"
+        />
+        <meta property="og:url" content="/" />
+        <meta property="og:type" content="website" />
+      </Head>
+      {/* <Helmet encodeSpecialCharacters={true}>
 				<script type="application/ld+json">
 					{JSON.stringify(jsonLd)}
 				</script>
 			</Helmet> */}
-			<div className=" ">
-				<HeadingSection
-					className="pt-uui-6xl"
-					title={m.leaderboard_heading_title()}
-					subtitle={m.leaderboard_heading_subtitle()}
-				/>
-				<SectionContainer className="sm:pt-uui-container-padding-desktop mx-auto pt-uui-container-padding-mobile">
-					{/* Set max height & min height to make table scrollable & minimize layout shifts on state changes */}
-					<Table className="max-h-[70vh] min-h-[70vh] relative">
-						{{
-							cardHeader: (
-								<CardHeader
-									className="sticky bg-uui-bg-primary left-0 right-0 top-0 z-10 box-border"
-									headerTitle={m.leaderboard_node_provider_table_cardHeader_headerTitle()}
-									subtitle={m.leaderboard_node_provider_table_cardHeader_subtitle()}
-									trailingField={
-										<div className="w-full md:w-[26.188rem] ">
-											<InputField
-												value={walletAddress}
-												onChange={(e) =>
-													setWalletAddress(
-														e.target.value
-													)
-												}
-												placeholder={m.leaderboard_node_provider_table_inputField_placeholder()}
-												iconUrl={generalSearchMd}
-											/>
-										</div>
-									}
-								/>
-							),
-							tableSubstitute:
-								isLoading ||
-								isError ||
-								data?.filter((d) =>
-									walletAddress
-										? d.Wallet.includes(walletAddress)
-										: true
-								).length === 0 ? (
-									<EmptyState
-										header={
-											isLoading
-												? m.leaderboard_node_provider_table_loadingState_loadingText()
-												: isError
-												? m.leaderboard_node_provider_table_errorState_errorText()
-												: m.leaderboard_node_provider_table_emptyState_emptyText()
-										}
-										description={
-											isLoading
-												? m.leaderboard_node_provider_table_loadingState_loadingHint()
-												: isError
-												? m.leaderboard_node_provider_table_errorState_errorHint()
-												: m.leaderboard_node_provider_table_emptyState_emptyHint()
-										}
-									>
-										<FeaturedIcon
-											spinIcon={isLoading}
-											iconUrl={
-												isLoading
-													? generalLoading01
-													: isError
-													? alertAndFeedbackAlertCircle
-													: generalSearchLg
-											}
-										/>
-									</EmptyState>
-								) : null,
-							tableRows: (
-								<>
-									<thead className="">
-										{/* Translate -translate-y-[0.063rem] to close the 1px padding gap when sticky */}
-										<tr className="sticky z-10 top-0 w-full after:w-full after:absolute after:inset-x-0 after:bottom-0 after:border-t-uui-1 after:border-t-uui-border-secondary">
-											{getHeaderData().map(
-												(
-													{
-														name,
-														translation,
-														tooltip,
-													},
-													i,
-													a
-												) => (
-													<th
-														key={name}
-														className="p-0"
-														colSpan={
-															i === a.length - 1
-																? 2
-																: 1
-														}
-													>
-														<TableHeaderCell
-															title={translation}
-														>
-															{tooltip ? (
-																<Tooltip
-																	text={
-																		tooltip.title
-																	}
-																	description={
-																		tooltip.description
-																	}
-																/>
-															) : null}
-														</TableHeaderCell>
-													</th>
-												)
-											)}
-										</tr>
-									</thead>
-									<tbody>
-										{toTableData(data ?? [])
-											.filter((d) =>
-												walletAddress
-													? d.Wallet.includes(
-															walletAddress
-													  )
-													: true
-											)
-											.map((row, rowIndex) => (
-												<tr key={rowIndex}>
-													<td>
-														<TableLeadText
-															title={row["Rank"]}
-														/>
-													</td>
-													<td>
-														<TableLeadText
-															title={
-																<Badge
-																	badgeType="Pill outline"
-																	color={
-																		row
-																			.Level
-																			.color
-																	}
-																	size="sm"
-																	icon={{
-																		type: "icon",
-																		leading:
-																			row
-																				.Level
-																				.icon,
-																	}}
-																>
-																	{
-																		row
-																			.Level
-																			.text
-																	}
-																</Badge>
-															}
-														></TableLeadText>
-													</td>
-													<td>
-														<TableLeadText
-															title={
-																row["Wallet"]
-															}
-														/>
-													</td>
-													<td>
-														<TableLeadText
-															title={
-																row[
-																	"Energy Provided"
-																]
-															}
-														/>
-													</td>
-													<td>
-														<TableLeadText
-															title={
-																row[
-																	"Reward Points"
-																]
-															}
-														/>
-													</td>
-													<td>
-														<TableLeadText
-															title={
-																<Badge
-																	badgeType="Badge modern"
-																	color={
-																		row[
-																			"Status"
-																		].color
-																	}
-																	size="sm"
-																	icon={{
-																		type: "dot",
-																	}}
-																>
-																	{
-																		row[
-																			"Status"
-																		]
-																			.translation
-																	}
-																</Badge>
-															}
-														/>
-													</td>
-													<td>
-														<TableLeadText
-															title={
-																<a
-																	href={row[
-																		"Share"
-																	].getUrl({
-																		currentUrl,
-																	})}
-																	target="_blank"
-																	rel="noopener noreferrer"
-																>
-																	<SocialIcon
-																		className="[&&]:h-uui-xl [&&]:w-uui-xl"
-																		iconUrl="/x.svg"
-																	/>
-																</a>
-															}
-														/>
-													</td>
-												</tr>
-											))}
-									</tbody>
-								</>
-							),
-							// Todo add in pagination after we receive the paginated API
-							// pagination: (
-							//   <div className=" sticky bg-uui-bg-primary left-0 right-0 bottom-0 flex justify-between items-center z-10">
-							//     <button>Left</button>
-							//     <h1>Header</h1>
-							//     <button>Right</button>
-							//   </div>
-							// ),
-						}}
-					</Table>
-				</SectionContainer>
-			</div>
-		</>
-	);
+      <div className=" ">
+        <HeadingSection
+          className="pt-uui-6xl"
+          title={m.leaderboard_heading_title()}
+          subtitle={m.leaderboard_heading_subtitle()}
+        />
+        <SectionContainer className="sm:pt-uui-container-padding-desktop mx-auto pt-uui-container-padding-mobile">
+          {/* Set max height & min height to make table scrollable & minimize layout shifts on state changes */}
+          <Table className="max-h-[70vh] min-h-[70vh] relative">
+            {{
+              cardHeader: (
+                <CardHeader
+                  className="sticky bg-uui-bg-primary left-0 right-0 top-0 z-10 box-border"
+                  headerTitle={m.leaderboard_node_provider_table_cardHeader_headerTitle()}
+                  subtitle={m.leaderboard_node_provider_table_cardHeader_subtitle()}
+                  trailingField={
+                    <div className="w-full md:w-[26.188rem] ">
+                      <InputField
+                        value={walletAddress}
+                        onChange={(e) => setWalletAddress(e.target.value)}
+                        placeholder={m.leaderboard_node_provider_table_inputField_placeholder()}
+                        iconUrl={generalSearchMd}
+                      />
+                    </div>
+                  }
+                />
+              ),
+              tableSubstitute:
+                isLoading ||
+                isError ||
+                data?.filter((d) =>
+                  walletAddress ? d.Wallet.includes(walletAddress) : true
+                ).length === 0 ? (
+                  <EmptyState
+                    header={
+                      isLoading
+                        ? m.leaderboard_node_provider_table_loadingState_loadingText()
+                        : isError
+                        ? m.leaderboard_node_provider_table_errorState_errorText()
+                        : m.leaderboard_node_provider_table_emptyState_emptyText()
+                    }
+                    description={
+                      isLoading
+                        ? m.leaderboard_node_provider_table_loadingState_loadingHint()
+                        : isError
+                        ? m.leaderboard_node_provider_table_errorState_errorHint()
+                        : m.leaderboard_node_provider_table_emptyState_emptyHint()
+                    }
+                  >
+                    <FeaturedIcon
+                      spinIcon={isLoading}
+                      iconUrl={
+                        isLoading
+                          ? generalLoading01
+                          : isError
+                          ? alertAndFeedbackAlertCircle
+                          : generalSearchLg
+                      }
+                    />
+                  </EmptyState>
+                ) : null,
+              tableRows: (
+                <>
+                  <thead className="">
+                    {/* Translate -translate-y-[0.063rem] to close the 1px padding gap when sticky */}
+                    <tr className="sticky z-10 top-0 w-full after:w-full after:absolute after:inset-x-0 after:bottom-0 after:border-t-uui-1 after:border-t-uui-border-secondary">
+                      {getHeaderData().map(
+                        ({ name, translation, tooltip }, i, a) => (
+                          <th
+                            key={name}
+                            className="p-0"
+                            colSpan={i === a.length - 1 ? 2 : 1}
+                          >
+                            <TableHeaderCell title={translation}>
+                              {tooltip ? (
+                                <Tooltip
+                                  text={tooltip.title}
+                                  description={tooltip.description}
+                                />
+                              ) : null}
+                            </TableHeaderCell>
+                          </th>
+                        )
+                      )}
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {toTableData(data ?? [])
+                      .filter((d) =>
+                        walletAddress ? d.Wallet.includes(walletAddress) : true
+                      )
+                      .map((row, rowIndex) => (
+                        <tr key={rowIndex}>
+                          <td>
+                            <TableLeadText title={row["Rank"]} />
+                          </td>
+                          <td>
+                            <TableLeadText
+                              title={
+                                <Badge
+                                  badgeType="Pill outline"
+                                  color={row.Level.color}
+                                  size="sm"
+                                  icon={{
+                                    type: "icon",
+                                    leading: row.Level.icon,
+                                  }}
+                                >
+                                  {row.Level.text}
+                                </Badge>
+                              }
+                            ></TableLeadText>
+                          </td>
+                          <td>
+                            <TableLeadText title={row["Wallet"]} />
+                          </td>
+                          <td>
+                            <TableLeadText title={row["Energy Provided"]} />
+                          </td>
+                          <td>
+                            <TableLeadText title={row["Reward Points"]} />
+                          </td>
+                          <td>
+                            <TableLeadText
+                              title={
+                                <Badge
+                                  badgeType="Badge modern"
+                                  color={row["Status"].color}
+                                  size="sm"
+                                  icon={{
+                                    type: "dot",
+                                  }}
+                                >
+                                  {row["Status"].translation}
+                                </Badge>
+                              }
+                            />
+                          </td>
+                          <td>
+                            <TableLeadText
+                              title={
+                                <a
+                                  href={row["Share"].getUrl({
+                                    currentUrl,
+                                    currentLevel: row.Level.text,
+                                  })}
+                                  target="_blank"
+                                  rel="noopener noreferrer"
+                                >
+                                  <SocialIcon
+                                    className="[&&]:h-uui-xl [&&]:w-uui-xl"
+                                    iconUrl="/x.svg"
+                                  />
+                                </a>
+                              }
+                            />
+                          </td>
+                        </tr>
+                      ))}
+                  </tbody>
+                </>
+              ),
+              // Todo add in pagination after we receive the paginated API
+              // pagination: (
+              //   <div className=" sticky bg-uui-bg-primary left-0 right-0 bottom-0 flex justify-between items-center z-10">
+              //     <button>Left</button>
+              //     <h1>Header</h1>
+              //     <button>Right</button>
+              //   </div>
+              // ),
+            }}
+          </Table>
+        </SectionContainer>
+      </div>
+    </>
+  );
 }
--- apps/info-dashboard/src/lib/fetchers/leaderboard.ts
+++ apps/info-dashboard/src/lib/fetchers/leaderboard.ts
@@ -1,131 +1,146 @@
 import { ascending, sort } from "d3";
 export type LeaderboardReturnType = {
-	Points: string;
-	Rank: string;
-	Energy: string;
-	Wallet: string;
+  Points: string;
+  Rank: string;
+  Energy: string;
+  Wallet: string;
 }[];
 import * as m from "@/paraglide/messages.js";
 import {
-	developmentCpuChip02,
-	editorMagicWand02,
-	financeAndEcommerceDiamond01,
-	weatherLightning02,
+  developmentCpuChip02,
+  editorMagicWand02,
+  financeAndEcommerceDiamond01,
+  weatherLightning02,
 } from "@frontline-hq/untitledui-icons";
 
 export async function fetchLeaderboard() {
-	//const api_host = process.env.NEXT_PUBLIC_API_HOST;
-	const api_host = "https://api-testnet.lilypad.tech/";
-	const leaderboard_url = `${api_host}metrics-dashboard/leaderboard`;
-	const raw = await fetch(leaderboard_url);
-	return (await raw.json()) as LeaderboardReturnType;
+  //const api_host = process.env.NEXT_PUBLIC_API_HOST;
+  const api_host = "https://api-testnet.lilypad.tech/";
+  const leaderboard_url = `${api_host}metrics-dashboard/leaderboard`;
+  const raw = await fetch(leaderboard_url);
+  return (await raw.json()) as LeaderboardReturnType;
 }
 
 export function toTableData(data: LeaderboardReturnType) {
-	return sort(data, (a, b) => ascending(Number(a.Rank), Number(b.Rank))).map(
-		({ Rank, Wallet, Energy, Points }) => ({
-			Rank,
-			Wallet,
-			Level: (() => {
-				const rankNumber = Number(Rank);
-				const result =
-					rankNumber < 5
-						? ({
-								color: "warning",
-								icon: weatherLightning02,
-								text: m.leaderboard_node_provider_table_first_level(),
-						  } as const)
-						: rankNumber < 15
-						? ({
-								color: "pink",
-								icon: financeAndEcommerceDiamond01,
-								text: m.leaderboard_node_provider_table_second_level(),
-						  } as const)
-						: rankNumber < 35
-						? ({
-								color: "brand",
-								icon: editorMagicWand02,
-								text: m.leaderboard_node_provider_table_third_level(),
-						  } as const)
-						: ({
-								color: "gray",
-								icon: developmentCpuChip02,
-								text: m.leaderboard_node_provider_table_last_level(),
-						  } as const);
-				return result;
-			})(),
-			"Energy Provided": Energy,
-			"Reward Points": Points,
-			Status: (() => {
-				const online = Math.random() > 0.5 ? true : false;
-				return {
-					online,
-					color: online ? "success" : "error",
-					translation: online
-						? m.leaderboard_node_provider_table_online_status()
-						: m.leaderboard_node_provider_table_offline_status(),
-				} as const;
-			})(),
-			Share: {
-				translation: "share",
-				getUrl: ({ currentUrl }: { currentUrl: string }) => {
-					const normalShareText = encodeURIComponent(
-						m.leaderboard_node_provider_table_share_x_tweet_shareText()
-					);
-					return `https://twitter.com/intent/tweet?text=${normalShareText}&url=${currentUrl}?wallet_id=${Wallet}`;
-				},
-			},
-		})
-	);
+  return sort(data, (a, b) => ascending(Number(a.Rank), Number(b.Rank))).map(
+    ({ Rank, Wallet, Energy, Points }) => ({
+      Rank,
+      Wallet,
+      Level: (() => {
+        const rankNumber = Number(Rank);
+        const result =
+          rankNumber < 6
+            ? ({
+                color: "warning",
+                icon: weatherLightning02,
+                text: m.leaderboard_node_provider_table_first_level(),
+              } as const)
+            : rankNumber < 16
+            ? ({
+                color: "pink",
+                icon: financeAndEcommerceDiamond01,
+                text: m.leaderboard_node_provider_table_second_level(),
+              } as const)
+            : rankNumber < 36
+            ? ({
+                color: "brand",
+                icon: editorMagicWand02,
+                text: m.leaderboard_node_provider_table_third_level(),
+              } as const)
+            : ({
+                color: "gray",
+                icon: developmentCpuChip02,
+                text: m.leaderboard_node_provider_table_last_level(),
+              } as const);
+        return result;
+      })(),
+      "Energy Provided": Energy,
+      "Reward Points": Points,
+      Status: (() => {
+        const online = Math.random() > 0.5 ? true : false;
+        return {
+          online,
+          color: online ? "success" : "error",
+          translation: online
+            ? m.leaderboard_node_provider_table_online_status()
+            : m.leaderboard_node_provider_table_offline_status(),
+        } as const;
+      })(),
+      Share: {
+        translation: "share",
+        getUrl: ({
+          currentUrl,
+          currentLevel,
+        }: {
+          currentUrl: string;
+          currentLevel: string;
+        }) => {
+          const linkText =
+            m.leaderboard_node_provider_table_share_x_tweet_shareText() +
+            "\n" +
+            m.leaderboard_node_provider_table_share_x_tweet_shareText2() +
+            currentLevel +
+            (currentLevel.includes("Titan")
+              ? " ⚡️"
+              : currentLevel.includes("Legend")
+              ? " 💎"
+              : currentLevel.includes("Wizard")
+              ? " 🪄"
+              : " 🤖" + "!") +
+            "\n";
+          const normalShareText = encodeURIComponent(linkText);
+          return `https://twitter.com/intent/tweet?text=${normalShareText}&url=${currentUrl}?wallet_id=${Wallet}`;
+        },
+      },
+    })
+  );
 }
 
 export function getHeaderData() {
-	return [
-		{
-			name: "Rank",
-			translation: m.leaderboard_header_titles_rank(),
-			tooltip: undefined,
-		},
-		{
-			name: "Level",
-			translation: m.leaderboard_header_titles_level(),
-			tooltip: {
-				title: m.leaderboard_header_tooltip_title_level(),
-				description: m.leaderboard_header_tooltip_description_level(),
-			},
-		},
-		{
-			name: "Wallet",
-			translation: m.leaderboard_header_titles_wallet(),
-			tooltip: undefined,
-		},
-		{
-			name: "Energy Provided",
-			translation: m.leaderboard_header_titles_energy_provided(),
-			tooltip: {
-				title: m.leaderboard_header_tooltip_title_energy_provided(),
-				description:
-					m.leaderboard_header_tooltip_description_energy_provided(),
-			},
-		},
-		{
-			name: "Reward Points",
-			translation: m.leaderboard_header_titles_reward_points(),
-			tooltip: {
-				title: m.leaderboard_header_tooltip_title_reward_points(),
-				description:
-					m.leaderboard_header_tooltip_description_reward_points(),
-			},
-		},
-		{
-			name: "Status",
-			translation: m.leaderboard_header_titles_status(),
-			tooltip: undefined,
-		},
-		{
-			name: "Share",
-			translation: m.leaderboard_header_titles_share(),
-			tooltip: undefined,
-		},
-	] as const;
+  return [
+    {
+      name: "Rank",
+      translation: m.leaderboard_header_titles_rank(),
+      tooltip: undefined,
+    },
+    {
+      name: "Level",
+      translation: m.leaderboard_header_titles_level(),
+      tooltip: {
+        title: m.leaderboard_header_tooltip_title_level(),
+        description: m.leaderboard_header_tooltip_description_level(),
+      },
+    },
+    {
+      name: "Wallet",
+      translation: m.leaderboard_header_titles_wallet(),
+      tooltip: undefined,
+    },
+    {
+      name: "Energy Provided",
+      translation: m.leaderboard_header_titles_energy_provided(),
+      tooltip: {
+        title: m.leaderboard_header_tooltip_title_energy_provided(),
+        description: m.leaderboard_header_tooltip_description_energy_provided(),
+      },
+    },
+    {
+      name: "Reward Points",
+      translation: m.leaderboard_header_titles_reward_points(),
+      tooltip: {
+        title: m.leaderboard_header_tooltip_title_reward_points(),
+        description: m.leaderboard_header_tooltip_description_reward_points(),
+      },
+    },
+    {
+      name: "Status",
+      translation: m.leaderboard_header_titles_status(),
+      tooltip: undefined,
+    },
+    {
+      name: "Share",
+      translation: m.leaderboard_header_titles_share(),
+      tooltip: undefined,
+    },
+  ] as const;
 }
