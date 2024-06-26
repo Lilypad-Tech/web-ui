515cc28ee3caab688065507640f4a4439866b1be
Nadiem <nadiem8@msn.com>
2024-06-26 09:28:16 +0200

    fix:add subtitle to CardWithBorder and make title optional

--- apps/info-dashboard/src/components/CardWithBorder/CardWithBorder.tsx
+++ apps/info-dashboard/src/components/CardWithBorder/CardWithBorder.tsx
@@ -1,15 +1,30 @@
 interface CardWithBorderProps extends React.HTMLAttributes<HTMLDivElement> {
-	title: string;
+	title?: string;
+	subtitle?: string;
 }
 
 export default function CardWithBorder({
 	title,
+	subtitle,
 	children,
+	...props
 }: CardWithBorderProps) {
 	return (
-		<div className="p-uui-2xl uui-desktop:p-uui-3xl gap-uui-2xl w-full snap-center h-full rounded-uui-xl border-uui-1 border-uui-border-secondary flex flex-col justify-between bg-uui-bg-primary_alt">
-			<div className="uui-text-lg text-uui-text-primary-900 font-semibold">
-				{title}
+		<div
+			{...props}
+			className={`${props.className} p-uui-2xl uui-desktop:p-uui-3xl gap-uui-2xl w-full snap-center rounded-uui-xl border-uui-1 border-uui-border-secondary flex flex-col justify-between bg-uui-bg-primary_alt `}
+		>
+			<div className="flex flex-col space-y-uui-xs">
+				{title && (
+					<div className="uui-text-lg text-uui-text-primary-900 font-semibold antialiased">
+						{title}
+					</div>
+				)}
+				{subtitle && (
+					<div className="uui-text-sm antialiased font-regular text-uui-text-tertiary-600">
+						{subtitle}
+					</div>
+				)}
 			</div>
 			{children}
 		</div>
