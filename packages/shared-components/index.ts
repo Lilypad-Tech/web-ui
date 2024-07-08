7c811a386178a4b5cb0970528b8cdfe6ae5fee06
Nadiem <nadiem8@msn.com>
2024-07-08 12:02:11 +0200

    feat:Add SocialIcon component to shared-components library

--- packages/shared-components/index.ts
+++ packages/shared-components/index.ts
@@ -1,4 +1,5 @@
 export * from "./lib/Badge/index";
 export * from "./lib/SectionContainer/index";
 export * from "./lib/Anchor/index";
+export * from "./lib/SocialIcon/index";
 export * from "./lib/types";
