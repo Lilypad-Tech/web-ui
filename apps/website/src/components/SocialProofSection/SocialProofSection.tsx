5f5692a320470460ec164cdf9e54228fd764f6e5
Nadiem <nadiem8@msn.com>
2024-07-09 15:51:22 +0200

    feat:add SocialProofSection

---
+++ apps/website/src/components/SocialProofSection/SocialProofSection.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import { HTMLAttributes } from "react";
+
+interface SocialProofSectionProps extends HTMLAttributes<HTMLDivElement> {
+	trustedByArray: Array<{ src: string; alt: string }>;
+	title: string;
+}
+
+const SocialProofSection = ({
+	trustedByArray,
+	title,
+	...props
+}: SocialProofSectionProps) => {
+	return (
+		<div
+			{...props}
+			className={`${props.className} flex flex-col w-full items-center space-y-uui-2xl lg:space-y-uui-4xl`}
+		>
+			<span className="uui-text-sm md:uui-text-md font-medium text-uui-text-primary-900 antialiased">
+				{title}
+			</span>
+			<div className="flex overflow-x-auto no-scrollbar gap-uui-4xl lg:gap-uui-6xl w-full items-center justify-between pb-uui-7xl lg:pb-uui-8xl">
+				{trustedByArray.map(({ src, alt }, index) => {
+					return (
+						<Image
+							height={400}
+							width={400}
+							key={index}
+							className="h-6 md:h-8 w-auto"
+							src={src}
+							alt={alt}
+						/>
+					);
+				})}
+			</div>
+		</div>
+	);
+};
+export default SocialProofSection;
