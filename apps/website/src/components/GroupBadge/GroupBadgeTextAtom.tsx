95ef7cd1a25d0f265f54dde81735a638e6266d39
Nadiem <nadiem8@msn.com>
2024-07-09 13:18:42 +0200

    feat:add GroupBadge to the website components

---
+++ apps/website/src/components/GroupBadge/GroupBadgeTextAtom.tsx
@@ -0,0 +1,23 @@
+import { HTMLAttributes } from "react";
+import { GroupBadgeSizes } from "./types";
+
+interface GroupBadgeTextAtomProps extends HTMLAttributes<HTMLDivElement> {
+	size: GroupBadgeSizes;
+}
+
+const GroupBadgeTextAtom = ({ children, size }: GroupBadgeTextAtomProps) => {
+	const fontStyles = {
+		md: "uui-text-xs",
+		lg: "uui-text-sm",
+	};
+
+	return (
+		<div
+			className={` ${fontStyles[size]} whitespace-nowrap antialiased font-medium truncate overflow-hidden max-w-[60vw] md:max-w-[25rem] `}
+		>
+			{children}
+		</div>
+	);
+};
+
+export default GroupBadgeTextAtom;
