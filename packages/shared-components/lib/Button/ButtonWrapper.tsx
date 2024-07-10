c5efc0cc57842d632dd640312ad155e6cb8a5506
Nadiem <nadiem8@msn.com>
2024-07-10 13:07:03 +0200

    feat: add button component to shared components

---
+++ packages/shared-components/lib/Button/ButtonWrapper.tsx
@@ -0,0 +1,88 @@
+import type { ButtonHTMLAttributes } from "react";
+import type {
+	ButtonColor,
+	ButtonDestructive,
+	ButtonHierarchy,
+	ButtonSize,
+} from "./ButtonTypes";
+
+interface ButtonWrapperProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+	size: ButtonSize;
+	destructive: ButtonDestructive;
+	color: ButtonColor;
+	hierarchy: ButtonHierarchy;
+}
+
+type Coloring = {
+	[D in `${ButtonDestructive}`]: {
+		[H in ButtonHierarchy]: {
+			color: string[];
+		};
+	};
+};
+
+export const ButtonWrapper = ({
+	children,
+	size,
+	destructive,
+	color,
+	hierarchy,
+	...props
+}: ButtonWrapperProps) => {
+	const layer1 =
+		" group flex justify-center items-center [&.disabled]:pointer-events-none uui-focus-all:outline-none [&.disabled]:text-uui-fg-disabled [&.disabled]:pointer-events-none ";
+	const shadow = " shadow-uui-xs ";
+
+	const wrapperSizes = {
+		md: " p-[var(--uui-spacing-2-5)] gap-uui-xs px-[var(--uui-spacing-3-5)] ",
+		xl: " py-uui-lg px-[var(--uui-spacing-4-5)] gap-uui-sm p-[var(--uui-spacing-2-5)] ",
+		"2xl": " px-[var(--uui-spacing-5-5)] gap-[var(--uui-spacing-2-5)] p-uui-xl",
+	};
+
+	const coloring = {
+		false: {
+			primary: {
+				color: [
+					"bg-uui-button-primary-bg",
+					"text-uui-button-primary-fg",
+					"border-uui-button-primary-bg",
+					"border",
+					"rounded-uui-3xl",
+					"border-solid",
+					// hover
+					"uui-hover-all:bg-uui-button-primary-bg_hover",
+					"uui-hover-all:text-uui-button-primary-fg_hover",
+					"uui-hover-all:border-uui-button-primary-border_hover",
+					// focus
+					"uui-focus-all:uui-ring-brand",
+					"uui-focus-all:shadow-uui-xs",
+					// disabled
+					"[&.disabled]:bg-uui-bg-disabled",
+					"[&.disabled]:border-uui-border-disabled_subtle",
+				],
+			},
+		},
+		// empty for now, will add in correct styles when needed
+		true: {
+			primary: {
+				color: [],
+			},
+		},
+	};
+
+	const destructiveColoring = destructive.toString() as keyof Coloring;
+
+	return (
+		<button
+			{...props}
+			onClick={props.onClick}
+			className={` ${props.className} ${layer1} ${shadow} ${
+				wrapperSizes[size]
+			} ${coloring[destructiveColoring][hierarchy][color].join(" ")}`}
+		>
+			{children}
+		</button>
+	);
+};
+
+export default ButtonWrapper;
