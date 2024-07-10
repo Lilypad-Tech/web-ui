36cc3fd72233bbf89c6136e4737fbc0328458fc1
Nadiem <nadiem8@msn.com>
2024-07-10 13:06:35 +0200

    feat: update InputField and move it from info-dashboard to the SharedComponents folder

---
+++ packages/shared-components/lib/InputField/InputHintAtom.tsx
@@ -0,0 +1,20 @@
+import type { HtmlHTMLAttributes } from "react";
+
+interface InputHintAtomProps extends HtmlHTMLAttributes<HTMLSpanElement> {
+	destructive?: boolean;
+}
+
+const InputHintAtom = ({ destructive, ...props }: InputHintAtomProps) => {
+	return (
+		<div
+			{...props}
+			className={`${props.className} ${
+				destructive ? "error" : ""
+			} antialiased text-uui-text-tertiary-600 font-regular uui-text-sm [&.error]:text-uui-text-error-primary-600 mt-uui-sm block text-left `}
+		>
+			{props.children}
+		</div>
+	);
+};
+
+export default InputHintAtom;
