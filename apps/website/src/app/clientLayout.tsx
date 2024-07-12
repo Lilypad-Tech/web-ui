8cc183c8901e710331267af8588f0feecf9c5647
Nadiem <nadiem8@msn.com>
2024-07-12 12:21:39 +0200

    feat: add mobile menu dropdown to the clientLayout navbar

--- apps/website/src/app/clientLayout.tsx
+++ apps/website/src/app/clientLayout.tsx
@@ -28,6 +28,9 @@ export default function ClientLayout({
 	children: React.ReactNode;
 }>) {
 	const [menuOpened, setMenuOpened] = useState(false);
+	const [nestedMenu, setNestedMenu] = useState<null | "About" | "Resources">(
+		null
+	);
 
 	const [bannerOpened, setBannerOpened] = useState(true);
 
@@ -56,8 +59,142 @@ export default function ClientLayout({
 							setOpened: setMenuOpened,
 						}}
 					>
+						{nestedMenu != null && menuOpened ? (
+							<button
+								className="mb-uui-2xl"
+								onClick={() => setNestedMenu(null)}
+							>
+								<_NavItemBase current={true}>
+									{"Back"}
+								</_NavItemBase>
+							</button>
+						) : null}
 						{menuOpened ? (
-							<button>hello</button>
+							<>
+								{nestedMenu === null && (
+									<>
+										<button
+											onClick={() => {
+												setNestedMenu("About");
+											}}
+										>
+											<_NavItemBase
+												// TODO add all paths that have to do with the about page
+												current={pathname === "/about"}
+											>
+												{"About"}
+											</_NavItemBase>
+										</button>
+										<Link
+											href="/#products"
+											onClick={() => {
+												setMenuOpened(() => false);
+											}}
+										>
+											<_NavItemBase
+												current={pathname === "/"}
+											>
+												{"Products"}
+											</_NavItemBase>
+										</Link>
+										<button
+											onClick={() => {
+												setNestedMenu("Resources");
+											}}
+										>
+											<_NavItemBase
+												// TODO add all paths that have to do with the about page
+												current={
+													pathname === "/resources"
+												}
+											>
+												{"Resources"}
+											</_NavItemBase>
+										</button>
+
+										<Link
+											href="/docs"
+											onClick={() => {
+												setMenuOpened(() => false);
+											}}
+										>
+											<_NavItemBase
+												current={pathname === "/docs"}
+											>
+												{"Docs"}
+											</_NavItemBase>
+										</Link>
+									</>
+								)}
+								{nestedMenu === "About" ? (
+									<div className="overflow-auto no-scrollbar max-h-[50vh] flex flex-col gap-uui-xl border-uui-1 border-uui-border-secondary rounded-xl p-uui-lg">
+										<_NavMenuItem
+											description="The latest industry news, updates and info"
+											title="About us"
+											iconUrl={"/lilypad-icon.svg"}
+											href="/test"
+										></_NavMenuItem>
+										<_NavMenuItem
+											description="The latest industry news, updates and info"
+											title="Our team"
+											iconUrl={usersUsers03}
+											href="/test"
+										></_NavMenuItem>
+										<_NavMenuItem
+											description="The latest industry news, updates and info"
+											title="Roadmap"
+											iconUrl={mapsAndTravelRoute}
+											href="/#products"
+										></_NavMenuItem>
+										<_NavMenuItem
+											description="The latest industry news, updates and info"
+											title="Press/Media"
+											iconUrl={mediaAndDevicesTv03}
+											href="/#products"
+										></_NavMenuItem>
+										<_NavMenuItem
+											description="The latest industry news, updates and info"
+											title="Use Cases"
+											iconUrl={mediaAndDevicesLightbulb05}
+											href="/#products"
+										></_NavMenuItem>
+									</div>
+								) : nestedMenu === "Resources" ? (
+									<div className="overflow-auto no-scrollbar max-h-[50vh] flex flex-col gap-uui-xl border-uui-1 border-uui-border-secondary rounded-xl p-uui-lg">
+										<_NavMenuItem
+											description="The latest industry news, updates and info"
+											title="Discord"
+											iconUrl={"/discord.svg"}
+											target="_blank"
+											href="https://discord.gg/lilypad-network"
+										></_NavMenuItem>
+										<_NavMenuItem
+											description="The latest industry news, updates and info"
+											title="Blog"
+											iconUrl={educationBookClosed}
+											href="/test"
+										></_NavMenuItem>
+										<_NavMenuItem
+											description="The latest industry news, updates and info"
+											title="Github"
+											iconUrl={"/github.svg"}
+											href="https://github.com/Lilypad-Tech"
+										></_NavMenuItem>
+										<_NavMenuItem
+											description="The latest industry news, updates and info"
+											title="Media Kit"
+											iconUrl={imagesCamera01}
+											href="/media-kit"
+										></_NavMenuItem>
+										<_NavMenuItem
+											description="The latest industry news, updates and info"
+											title="FAQ"
+											iconUrl={generalHelpCircle}
+											href="/faq"
+										></_NavMenuItem>
+									</div>
+								) : null}
+							</>
 						) : (
 							<>
 								<_NavItemDropdown
