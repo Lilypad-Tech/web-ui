81d247b1574aa20c0eca44405ee973a4923829c8
Nadiem <nadiem8@msn.com>
2024-07-18 16:41:12 +0200

    feat: add leading and trailing to button, cleanup button component

--- packages/shared-components/lib/Button/index.ts
+++ packages/shared-components/lib/Button/index.ts
@@ -1,4 +1,2 @@
 export { default as Button } from "./Button";
 export { default as ButtonWrapper } from "./ButtonWrapper";
-export { default as ButtonTextAtom } from "./ButtonTextAtom";
-export * from "./ButtonTypes";
