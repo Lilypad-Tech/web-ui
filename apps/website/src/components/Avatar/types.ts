fcf25bb62a2a5ad080602f3d7f2aa45ce87486f0
Nadiem <nadiem8@msn.com>
2024-07-22 14:03:31 +0200

    feat: add simple avatar component

---
+++ apps/website/src/components/Avatar/types.ts
@@ -0,0 +1,12 @@
+import { ImgHTMLAttributes } from "react";
+
+export type ObjectFit = "object-top" | "object-center" | "object-bottom";
+
+export interface AvatarImageProps extends ImgHTMLAttributes<HTMLImageElement> {
+	type: "default";
+	objectFit?: ObjectFit;
+	src: string;
+	alt: string;
+}
+
+export type AvatarSizes = "lg" | "xl" | "2xl" | "team-member-card";
