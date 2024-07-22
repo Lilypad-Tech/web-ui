5f07f36fc7e798c24fb783d10077c9aab6b89e66
Nadiem <nadiem8@msn.com>
2024-07-22 14:03:31 +0200

    feat: add simple avatar component

--- apps/website/src/components/Avatar/Avatar.tsx
+++ apps/website/src/components/Avatar/Avatar.tsx
@@ -1,13 +1,40 @@
-const Avatar = ({}) => {
+import { ButtonHTMLAttributes } from "react";
+import AvatarWrapper from "./AvatarWrapper";
+import type { AvatarImageProps, AvatarSizes, ObjectFit } from "./types";
+import Image from "next/image";
+
+interface AvatarProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+	size: AvatarSizes;
+	avatarImageProps: AvatarImageProps;
+}
+
+const Avatar = ({ size, avatarImageProps, ...props }: AvatarProps) => {
+	const avatarImageSizes = {
+		lg: "h-[3rem] w-[3rem]",
+		xl: "h-[3.5rem] w-[3.5rem]",
+		"2xl": "h-[4rem] w-[4rem]",
+		"team-member-card": "h-[5rem] w-[5rem] md:h-[6rem] md:w-[6rem]",
+	};
+	console.log(props.onClick, "props");
 	return (
-		// TODO this is the avataratom
-		<div className={avatarSizes}>
-			<img
-				src={src}
-				alt="Avatar"
-				className=" 
-		rounded-full border-uui-avatar-contrast-border object-cover bg-center bg-contain bg-no-repeat"
-			/>
-		</div>
+		<AvatarWrapper {...props}>
+			{{
+				default: (
+					<div
+						className={` ${avatarImageSizes[size]} rounded-full relative overflow-hidden`}
+					>
+						<Image
+							layout="fill"
+							src={avatarImageProps?.src}
+							alt={avatarImageProps?.alt}
+							className={` w-full h-full
+		rounded-full border-uui-avatar-contrast-border object-cover ${avatarImageProps?.objectFit} bg-center bg-contain bg-no-repeat`}
+						/>
+					</div>
+				),
+			}}
+		</AvatarWrapper>
 	);
 };
+
+export default Avatar;
