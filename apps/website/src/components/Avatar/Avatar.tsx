6dccc81625f0c1527aa8bbfd74e48a951bc5543e
Nadiem <nadiem8@msn.com>
2024-07-22 10:26:17 +0200

    feat: start adding teams page

---
+++ apps/website/src/components/Avatar/Avatar.tsx
@@ -0,0 +1,13 @@
+const Avatar = ({}) => {
+	return (
+		// TODO this is the avataratom
+		<div className={avatarSizes}>
+			<img
+				src={src}
+				alt="Avatar"
+				className=" 
+		rounded-full border-uui-avatar-contrast-border object-cover bg-center bg-contain bg-no-repeat"
+			/>
+		</div>
+	);
+};
