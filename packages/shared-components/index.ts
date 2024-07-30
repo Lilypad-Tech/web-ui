e3ee05e4d9a81e44fdd9b20a35b1b274e403ec38
Benjamin Preiss <benjaminpreiss@protonmail.com>
2024-07-30 18:45:07 +0200

    fix: Add fallback UI for webgl init failure

--- packages/shared-components/index.ts
+++ packages/shared-components/index.ts
@@ -1,4 +1,6 @@
 export * from "./lib/Badge/index";
 export * from "./lib/SectionContainer/index";
 export * from "./lib/Anchor/index";
+export * from "./lib/ErrorBoundary/index";
+export * from "./lib/WebGLFallback/index";
 export * from "./lib/types";
