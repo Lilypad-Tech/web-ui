1a9dc835dffb28b22eb14f4a8c4fc090483af092
Nadiem <nadiem8@msn.com>
2024-08-12 16:02:02 +0200

    feat: add Roadmap, RoadmapItem and RoadmapItemWrapper

--- apps/website/src/components/Roadmap/RoadmapItem.tsx
+++ apps/website/src/components/Roadmap/RoadmapItem.tsx
@@ -1,10 +1,12 @@
-import { HTMLProps } from "react";
+import React, { HTMLProps } from "react";
+import { useSpring, animated } from "@react-spring/web";
 
-interface RoadmapProps extends HTMLProps<HTMLDivElement> {
+interface RoadmapItemProps extends HTMLProps<HTMLDivElement> {
 	header: string;
 	subtitle: string;
 	title: string;
 	achieved?: boolean;
+	inView?: boolean;
 }
 
 const RoadmapItem = ({
@@ -12,9 +14,22 @@ const RoadmapItem = ({
 	title,
 	subtitle,
 	achieved = false,
-}: RoadmapProps) => {
+	inView = false,
+}: RoadmapItemProps) => {
+	const springProps = useSpring({
+		from: { opacity: 0, transform: "translateY(100%)" },
+		to: {
+			opacity: inView ? 1 : 0,
+			transform: inView ? "translateY(0%)" : "translateY(100%)",
+		},
+		config: { mass: 1, tension: 170, friction: 26 },
+	});
+
 	return (
-		<div className="flex flex-col space-y-uui-md">
+		<animated.div
+			style={springProps}
+			className="flex flex-col space-y-uui-md"
+		>
 			<p className="uui-text-xs lg:uui-text-sm antialiased font-semibold text-uui-bg-brand-secondary">
 				{header}
 			</p>
@@ -24,7 +39,7 @@ const RoadmapItem = ({
 			<p className="uui-text-sm lg:uui-text-md antialiased text-uui-text-tertiary-600 font-regular">
 				{subtitle}
 			</p>
-		</div>
+		</animated.div>
 	);
 };
 
