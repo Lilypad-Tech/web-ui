fa26c9b45b6165d66f4a325c2af4b1ced76ea41e
Nadiem <nadiem8@msn.com>
2024-08-12 16:01:37 +0200

    chore: add UseFade & UseFadeInView hooks

---
+++ apps/website/src/app/hooks/UseFade.ts
@@ -0,0 +1,13 @@
+import { useSpring } from "@react-spring/web";
+
+function useFade(config = { duration: 500 }) {
+	const fade = useSpring({
+		from: { opacity: 0 },
+		to: { opacity: 1 },
+		config,
+	});
+
+	return fade;
+}
+
+export default useFade;
