511e306d64d53d85f10c67a374ae3af09e7681eb
developersteve <steven@developersteve.com>
2024-10-09 19:14:57 +1000

    Newsletter integration

--- apps/website/next.config.mjs
+++ apps/website/next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: false,
-	output: "export",
+  reactStrictMode: false,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
