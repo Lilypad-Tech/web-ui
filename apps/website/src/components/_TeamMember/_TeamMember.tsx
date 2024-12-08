41caa1159f0a5e176a07a35cad60e90dbc051883
PBillingsby <philbillingsby@gmail.com>
2024-12-08 12:03:56 +1100

    add conditional render for social icons

--- apps/website/src/components/_TeamMember/_TeamMember.tsx
+++ apps/website/src/components/_TeamMember/_TeamMember.tsx
@@ -18,44 +18,55 @@ const TeamMember = ({
 	socialIcons,
 }: TeamMemberProps) => {
 	return (
-		<div className="text-center flex flex-col items-center justify-center max-w-[19rem] md:max-w-[15rem] ">
+		<div className="text-center flex flex-col items-center max-w-[19rem] md:max-w-[15rem] h-full">
 			{src && (
 				<Avatar
-					className="mb-uui-xl md:mb-uui-2xl"
+					className="md:mb-uui-2xl"
 					size="team-member-card"
 					avatarImageProps={{
 						type: "default",
 						alt: name ?? "Team Member",
 						src: src,
 					}}
-				></Avatar>
+				/>
 			)}
 
 			{name && (
-				<h3 className="uui-text-lg font-semibold antialiased text-uui-text-primary-900 ">
+				<h3 className="uui-text-lg font-semibold antialiased text-uui-text-primary-900">
 					{name}
 				</h3>
 			)}
+
 			{position && (
-				<p className="font-regular text-uui-text-brand-secondary-700 uui-text-md antialiased">
+				<p className="font-regular text-uui-text-brand-secondary-700 uui-text-sm antialiased">
 					{position}
 				</p>
 			)}
+
 			{description && (
-				<p className="mt-uui-md font-regular uui-text-md text-uui-text-tertiary-600 antialiased">
+				<p className="mt-uui-md font-regular uui-text-sm text-uui-text-tertiary-600 antialiased">
 					{description}
 				</p>
 			)}
-			{socialIcons && (
+
+			{socialIcons && socialIcons.length > 0 && (
 				<div className="flex items-center justify-center space-x-uui-xl mt-uui-xl">
-					{socialIcons.map((icon, index) => (
-						<a key={index} href={icon.href} target="_blank">
-							<SocialIcon iconUrl={icon.iconUrl}></SocialIcon>
-						</a>
-					))}
+					{socialIcons
+						.filter((icon) => icon.href)
+						.map((icon, index) => (
+							<a
+								key={index}
+								href={icon.href}
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<SocialIcon iconUrl={icon.iconUrl} />
+							</a>
+						))}
 				</div>
 			)}
 		</div>
 	);
 };
+
 export default TeamMember;
