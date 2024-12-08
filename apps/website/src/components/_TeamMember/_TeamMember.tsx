d539794e2b2b2a6d06f1889f21a6eddf4374f65c
PBillingsby <philbillingsby@gmail.com>
2024-12-08 12:03:56 +1100

    add conditional render for social icons

--- apps/website/src/components/_TeamMember/_TeamMember.tsx
+++ apps/website/src/components/_TeamMember/_TeamMember.tsx
@@ -3,59 +3,78 @@ import Avatar from "../Avatar/Avatar";
 import { SocialIcon } from "@lilypad/shared-components";
 
 interface TeamMemberProps extends HTMLAttributes<HTMLDivElement> {
-	src?: string;
-	name?: string;
-	position?: string;
-	description?: string;
-	socialIcons?: { iconUrl: string; href: string }[];
+  src?: string;
+  name?: string;
+  position?: string;
+  description?: string;
+  experience?: string;
+  socialIcons?: { iconUrl?: string; href?: string }[];
 }
 
 const TeamMember = ({
-	src,
-	name,
-	position,
-	description,
-	socialIcons,
+  src,
+  name,
+  position,
+  description,
+  experience,
+  socialIcons,
 }: TeamMemberProps) => {
-	return (
-		<div className="text-center flex flex-col items-center justify-center max-w-[19rem] md:max-w-[15rem] ">
-			{src && (
-				<Avatar
-					className="mb-uui-xl md:mb-uui-2xl"
-					size="team-member-card"
-					avatarImageProps={{
-						type: "default",
-						alt: name ?? "Team Member",
-						src: src,
-					}}
-				></Avatar>
-			)}
+  return (
+    <div className="flex flex-col items-center justify-between text-center bg-inherit p-4 rounded-lg h-full min-h-[24rem] max-w-[19rem] md:max-w-[19rem]">
+      {src && (
+        <Avatar
+          className="md:mb-uui-2xl"
+          size="team-member-card"
+          avatarImageProps={{
+            type: "default",
+            alt: name ?? "Team Member",
+            src: src,
+          }}
+        />
+      )}
 
-			{name && (
-				<h3 className="uui-text-lg font-semibold antialiased text-uui-text-primary-900 ">
-					{name}
-				</h3>
-			)}
-			{position && (
-				<p className="font-regular text-uui-text-brand-secondary-700 uui-text-md antialiased">
-					{position}
-				</p>
-			)}
-			{description && (
-				<p className="mt-uui-md font-regular uui-text-md text-uui-text-tertiary-600 antialiased">
-					{description}
-				</p>
-			)}
-			{socialIcons && (
-				<div className="flex items-center justify-center space-x-uui-xl mt-uui-xl">
-					{socialIcons.map((icon, index) => (
-						<a key={index} href={icon.href} target="_blank">
-							<SocialIcon iconUrl={icon.iconUrl}></SocialIcon>
-						</a>
-					))}
-				</div>
-			)}
-		</div>
-	);
+      {name && (
+        <h3 className="uui-text-lg font-semibold antialiased text-uui-text-primary-900">
+          {name}
+        </h3>
+      )}
+
+      {position && (
+        <p className="font-regular text-uui-text-brand-secondary-700 uui-text-sm antialiased">
+          {position}
+        </p>
+      )}
+
+      {description && (
+        <p className="mt-uui-md font-regular uui-text-sm text-uui-text-tertiary-600 antialiased">
+          {description}
+        </p>
+      )}
+
+      {experience && (
+        <p className="mt-uui-md font-regular uui-text-sm text-uui-text-tertiary-600 antialiased">
+          {experience}
+        </p>
+      )}
+
+      {socialIcons && socialIcons.length > 0 && (
+        <div className="flex items-center justify-center space-x-uui-xl mt-uui-xl">
+          {socialIcons
+            .filter((icon) => icon.href !== null)
+            .map((icon, index) => (
+              <a
+                key={index}
+                href={icon.href}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <SocialIcon iconUrl={icon.iconUrl} />
+              </a>
+            ))}
+        </div>
+      )}
+    </div>
+  );
 };
+
 export default TeamMember;
