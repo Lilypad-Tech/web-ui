2dfe8ed884e6f4d019c9596d60b7ea9a6c6f8c86
PBillingsby <philbillingsby@gmail.com>
2024-12-16 13:16:18 -0500

    add trusted partner hook call to social proofs

--- apps/website/src/components/SocialProofSection/SocialProofSection.tsx
+++ apps/website/src/components/SocialProofSection/SocialProofSection.tsx
@@ -1,13 +1,20 @@
+import { useContext } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { PageContext } from "../../app/clientLayout";
+import { HomePageCmsInfo } from "../../app/hooks/strapi/types";
 
 interface SocialProofSectionProps {
-	trustedByArray: Array<{ src: string; alt: string; href?: string }>;
 	title: string;
 }
 
-const SocialProofSection = ({ trustedByArray, title }: SocialProofSectionProps) => {
+const SocialProofSection = ({ title }: SocialProofSectionProps) => {
+	const { strapi } = useContext(PageContext) as { strapi: HomePageCmsInfo };
+
+	// Use trustedBies from the context
+	const trustedByArray = strapi?.trusted_bies || [];
+
 	const settings = {
 		dots: false,
 		infinite: true,
@@ -34,19 +41,14 @@ const SocialProofSection = ({ trustedByArray, title }: SocialProofSectionProps) 
 		],
 	};
 
-	console.log("----", title)
-
 	return (
 		<div className="flex flex-col w-full items-center space-y-uui-2xl lg:space-y-uui-4xl">
 			<span className="uui-text-sm md:uui-text-md font-medium text-uui-text-primary-900 antialiased">
 				{title}
 			</span>
 			<Slider {...settings} className="w-full">
-				{trustedByArray.map(({ src, alt, href }, index) => (
-					<div
-						key={index}
-						className="flex items-center px-4"
-					>
+				{trustedByArray?.map(({ src, alt, href }, index) => (
+					<div key={index} className="flex items-center px-4">
 						<a href={href} target="_blank" rel="noopener noreferrer">
 							<img
 								src={src}
