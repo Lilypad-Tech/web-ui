044376e959b875fc8d28752ab705649facf6e2af
PBillingsby <philbillingsby@gmail.com>
2024-12-16 13:15:02 -0500

    adjust avatar size

--- apps/website/src/components/Avatar/Avatar.tsx
+++ apps/website/src/components/Avatar/Avatar.tsx
@@ -2,18 +2,16 @@ import { ButtonHTMLAttributes } from "react";
 import AvatarWrapper from "./AvatarWrapper";
 import type { AvatarImageProps, AvatarSizes, ObjectFit } from "./types";
 import Image from "next/image";
-
 interface AvatarProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 	size: AvatarSizes;
 	avatarImageProps: AvatarImageProps;
 }
-
 const Avatar = ({ size, avatarImageProps, ...props }: AvatarProps) => {
 	const avatarImageSizes = {
 		lg: "h-[3rem] w-[3rem]",
 		xl: "h-[3.5rem] w-[3.5rem]",
 		"2xl": "h-[4rem] w-[4rem]",
-		"team-member-card": "h-[5rem] w-[5rem] md:h-[6rem] md:w-[6rem]",
+		"team-member-card": "h-[12rem] w-[12rem] md:h-[10rem] md:w-[10rem]",
 	};
 	return (
 		<AvatarWrapper {...props}>
@@ -26,7 +24,7 @@ const Avatar = ({ size, avatarImageProps, ...props }: AvatarProps) => {
 							fill
 							src={avatarImageProps?.src}
 							alt={avatarImageProps?.alt}
-							className={` w-full h-full
+							className={`w-full h-full
 		rounded-full border-uui-avatar-contrast-border object-cover ${avatarImageProps?.objectFit} bg-center bg-contain bg-no-repeat`}
 						/>
 					</div>
@@ -35,5 +33,4 @@ const Avatar = ({ size, avatarImageProps, ...props }: AvatarProps) => {
 		</AvatarWrapper>
 	);
 };
-
 export default Avatar;
