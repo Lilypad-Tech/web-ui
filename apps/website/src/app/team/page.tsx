3497e31a193577e03f39f6cf144a4f36377c378d
PBillingsby <philbillingsby@gmail.com>
2024-12-16 13:14:41 -0500

    update styling for team page

--- apps/website/src/app/team/page.tsx
+++ apps/website/src/app/team/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { SectionContainer } from "@lilypad/shared-components";
+import { Anchor, SectionContainer } from "@lilypad/shared-components";
 import Head from "next/head";
 import CenterHeadingSection from "@/components/CenterHeadingSection/CenterHeadingSection";
 import _TeamMember from "@/components/_TeamMember/_TeamMember";
@@ -12,33 +12,19 @@ import AnimateSpring from "@/components/AnimateSpring";
 import useFade from "../hooks/UseFade";
 import { useContext } from "react";
 import { PageContext } from "../clientLayout";
-import { TeamPageCmsInfo } from "../hooks/strapi/types";
-
+import { TeamPageCmsInfo, HomePageCmsInfo } from "../hooks/strapi/types";
+import _Advisor from "@/components/_TeamMember/_Advisor";
+import { CallToActions } from "@/components/FooterBlock/CallToActions";
 export default function Teams() {
-
-	const { strapi } = useContext(PageContext) as { strapi: TeamPageCmsInfo };
-
-	const teamMembers = strapi?.teamMembers || [];
-	const advisors = strapi?.advisors || [];
-	const partners = strapi?.partners || [];
-
-	const socialLinks = [
-		{ href: "twitter.com", iconUrl: "/x.svg" },
-
-		{
-			href: "linkedin.com",
-			iconUrl: "/linkedin.svg",
-		},
-		{
-			href: "https://frontline.codes",
-			iconUrl: mapsAndTravelGlobe02,
-		},
-	];
-
+	const { strapi: teamPageStrapi } = useContext(PageContext) as { strapi: TeamPageCmsInfo };
+	const context = useContext(PageContext);
+	const teamMembers = teamPageStrapi?.teamMembers || [];
+	const advisors = teamPageStrapi?.advisors || [];
+	const partners = teamPageStrapi?.partners || [];
 	const [teamSection2Ref, teamSection2Springs] = useFadeInView();
 	const [teamSection3Ref, teamSection3Springs] = useFadeInView();
 	const fadeLandingspage = useFade();
-
+	
 	return (
 		<>
 			<Head>
@@ -64,7 +50,6 @@ export default function Teams() {
 				<meta property="og:url" content="/" />
 				<meta property="og:type" content="website" />
 			</Head>
-
 			<main className="overflow-hidden relative ">
 				<div className="relative w-full overflow-hidden  max-w-uui-width-4xl mx-auto">
 					<div className="absolute top-0 min-w-uui-width-2xl -translate-x-[25%] sm:-translate-x-uui-none -translate-y-[8%]  left-0 w-full h-[40vh] -z-10">
@@ -79,38 +64,45 @@ export default function Teams() {
 							/>
 						</animated.div>
 					</div>
-
 					<SectionContainer>
 						<CenterHeadingSection
 							titleClassName="[&&]:uui-display-md [&&]:md:uui-display-lg "
 							className="[&&]:bg-transparent pt-uui-7xl lg:pt-uui-9xl relative"
-							header="Lilypad team"
-							title="Meet our innovators"
-							subtitle="Learn more about the company and the world-class team behind Lilypad."
+							header="The minds behind the mission"
+							title="Meet the Lilypad team"
+							subtitle="A multi-disciplinary, diverse team at the intersection of AI and crypto, building an accessible, community-driven network empowering next generation of open web innovation."
 						></CenterHeadingSection>
+						{/* TODO: Add diversity stats? */}
 					</SectionContainer>
 				</div>
-
 				<div className="bg-uui-bg-primary w-full">
 					<SectionContainer>
-						<div className="py-uui-7xl lg:py-uui-9xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-uui-4xl gap-y-uui-6xl justify-items-center">
+						<div className="py-uui-2xl lg:pt-uui-9xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-uui-4xl gap-y-uui-2xl justify-items-center">
 							{teamMembers.length > 0 ? (
-								teamMembers.map((member, index) => (
+								teamMembers
+									.sort((a, b) => a.order - b.order)
+									.map((member, index) => (
 									<AnimateSpring key={index}>
 										<_TeamMember
 											src={
-												member.Image && member.Image.url
-													? `https://webadmin.lilypad.team${member.Image.url}`
-													: "/default-image.png" // Use a default image if none exists
+												member.Image
+													? `https://webadmin.lilypad.team${member.Image}`
+													: "/default-image.png"
 											}
-											title={member.Name}
+											name={member.Name}
 											position={member.Title}
 											description={member.blurb}
-											socialIcons={[
-												{ href: `https://twitter.com/${member.twitter}`, iconUrl: "/x.svg" },
-												{ href: `https://linkedin.com/in/${member.linkedin}`, iconUrl: "/linkedin.svg" },
-												{ href: member.website, iconUrl: mapsAndTravelGlobe02 },
-											]}
+											experience={member.experience}
+											socialIcons={
+												[
+												  member.twitter
+													? { href: `https://twitter.com/${member.twitter}`, iconUrl: "/x.svg" }
+													: null,
+												  member.linkedin
+													? { href: `https://linkedin.com/in/${member.linkedin}`, iconUrl: "/linkedin.svg" }
+													: null,
+												].filter((icon): icon is { href: string; iconUrl: string } => Boolean(icon))
+											  }
 										/>
 									</AnimateSpring>
 								))
@@ -118,49 +110,64 @@ export default function Teams() {
 								<p>No team members found.</p>
 							)}
 						</div>
+						<div className="mx-auto flex flex-col gap-4 py-uui-4xl text-center">
+							<p className="text-uui-text-tertiary-600 uui-text-lg md:uui-text-xl font-regular">Want to learn more about our team?</p>
+							<Anchor
+								target="_blank"
+								href="https://lilypad.team/presskit"
+								className="[&&]:rounded-full w-full md:w-fit cursor-pointer mx-auto"
+								color="color"
+								destructive={false}
+								hierarchy="primary"
+								size="xl"
+							>
+								Visit Press Kit
+							</Anchor>
+						</div>
 					</SectionContainer>
-
 				</div>
 				<div className="bg-uui-bg-secondary">
 				  <SectionContainer>
 				    <CenterHeadingSection
-				      className="py-uui-7xl lg:py-uui-9xl"
+				      className="pt-uui-7xl lg:pt-uui-9xl"
 				      title="Advisors"
-				      subtitle="Our philosophy is simple — hire a team of diverse, passionate people and foster a culture that empowers you to do your best work."
+				      subtitle="The Lilypad team is proud to be supported by a network of visionary advisors who share our passion for decentralized AI and are committed to helping us build a better future for the web."
 				    ></CenterHeadingSection>
-
 				    <animated.div
 				      ref={teamSection2Ref}
 				      style={teamSection2Springs}
-				      className="pb-uui-7xl lg:pb-uui-9xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-uui-4xl gap-y-uui-6xl justify-items-center"
+				      className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-uui-4xl gap-y-uui-2xl justify-items-center"
 				    >
 				      {advisors.length > 0 ? (
-				        advisors.map((advisor, index) => (
-				          <_TeamMember
-				            key={index}
-				            src={advisor.Image?.url ? `https://webadmin.lilypad.team${advisor.Image.url}` : "/default-image.png"}
-				            title={advisor.Name}
-				            position={advisor.Title}
-				            description={advisor.blurb}
-				            socialIcons={[
-				              { href: `https://twitter.com/${advisor.twitter}`, iconUrl: "/x.svg" },
-				              { href: `https://linkedin.com/in/${advisor.linkedin}`, iconUrl: "/linkedin.svg" },
-				              { href: advisor.website, iconUrl: mapsAndTravelGlobe02 },
-				            ]}
-				          />
-				        ))
+				      advisors
+					  .sort((a, b) => a.order - b.order) // Sort by "order" property in ascending order
+					  .map((advisor, index) => {
+						return (
+						  <_Advisor
+							key={index}
+							src={advisor.Image ? `https://webadmin.lilypad.team${advisor.Image}` : "/default-image.png"}
+							title={advisor.Name}
+							position={advisor.Title}
+							experience={advisor.experience}
+							socialIcons={[
+							  { href: `https://twitter.com/${advisor.twitter}`, iconUrl: "/x.svg" },
+							  { href: `https://linkedin.com/in/${advisor.linkedin}`, iconUrl: "/linkedin.svg" },
+							]}
+						  />
+						);
+					  })
+					
 				      ) : (
 				        <p>No advisors found.</p>
 				      )}
 				    </animated.div>
 				  </SectionContainer>
 				</div>
-
 				<SectionContainer>
 				  <CenterHeadingSection
 				    className="[&&]:bg-uui-bg-primary pt-uui-7xl lg:pt-uui-9xl"
 				    title="Partners"
-				    subtitle="Our philosophy is simple — hire a team of diverse, passionate people and foster a culture that empowers you to do your best work."
+				    subtitle="Meet the skilled front-end engineers and tokenomics experts who are instrumental in building Lilypad's future."
 				  >
 				    <animated.div
 				      ref={teamSection3Ref}
@@ -169,15 +176,13 @@ export default function Teams() {
 				    >
 				      {partners.length > 0 ? (
 				        partners.map((partner, index) => (
-				          <_TeamMember
+				          <_Advisor
 				            key={index}
-					        src={partner.Image?.url ? `https://webadmin.lilypad.team${partner.Image.url}` : "/default-image.png"}
+					        src={partner.Image ? `https://webadmin.lilypad.team${partner.Image}` : "/default-image.png"}
 				            title={partner.Name}
 				            position={partner.Title}
 				            description={partner.blurb}
 				            socialIcons={[
-				              { href: `https://twitter.com/${partner.twitter}`, iconUrl: "/x.svg" },
-				              { href: `https://linkedin.com/in/${partner.linkedin}`, iconUrl: "/linkedin.svg" },
 				              { href: partner.website, iconUrl: mapsAndTravelGlobe02 },
 				            ]}
 				          />
@@ -186,9 +191,21 @@ export default function Teams() {
 				        <p>No partners found.</p>
 				      )}
 				    </animated.div>
-				  </CenterHeadingSection>
+				  	</CenterHeadingSection>
+					{/* TODO: Fix render bug */}
+				  	{/* <SectionContainer className="pb-uui-7xl lg:pb-uui-8xl">
+						<animated.div>
+							<SocialProofSection
+								title="Trusted by"
+							></SocialProofSection>
+						</animated.div>
+					</SectionContainer> */}
 				</SectionContainer>
-
+				<SectionContainer>
+					<div className="grid grid-cols-1 lg:grid-cols-2 py-uui-6xl gap-uui-3xl">
+						<CallToActions />
+					</div>
+				</SectionContainer>
 			</main>
 		</>
 	);
