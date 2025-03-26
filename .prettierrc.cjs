9f53fa5f4c4c1c8e03a359ddec84af798d5152df
Devlin <devlin@devlinrocha.com>
2025-03-26 14:43:20 -0700

    `npm run prettier`

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -3,11 +3,11 @@
  * @type {import("prettier").Config}
  */
 const config = {
-  trailingComma: "es5",
-  tabWidth: 4,
-  semi: false,
-  singleQuote: true,
-  plugins: ["prettier-plugin-tailwindcss"],
-};
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    plugins: ['prettier-plugin-tailwindcss'],
+}
 
-module.exports = config;
+module.exports = config
