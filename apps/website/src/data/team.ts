7ef0d7ebad92d03c600ffee645edddc5b2398d7e
Devlin <devlin@devlinrocha.com>
2025-03-28 15:40:41 -0700

    create initial `team.ts`

---
+++ apps/website/src/data/team.ts
@@ -0,0 +1,212 @@
+export const team = [
+    {
+        name: 'Alison Haire',
+        title: 'CEO & Founder',
+        description:
+            'Tech leader merging AI, blockchain, and creativity for real-world impact.',
+        experience: 'Filecoin, IBM, and Accenture',
+        image: '',
+        twitter: 'DeveloperAlly',
+        linkedin: 'Ally-Haire',
+        order: 1,
+    },
+    {
+        name: 'Dr. Stanley Bishop',
+        title: 'Co-Founder, Chief Scientists',
+        description:
+            'Mathematician and machine learning scientist specializing in data systems for scientific research.',
+        experience: 'AI & ML Engineer & Researcher, Stanford University',
+        image: '',
+        twitter: 'ScienceStanley',
+        linkedin: 'ScienceStanley',
+        order: 2,
+    },
+    {
+        name: 'Dr. Clint Snider',
+        title: 'Tokenomics Engineer',
+        description:
+            'Tokenomics expert specializing in crypto-economic system development. PhD in Systems Engineering.',
+        experience: 'University of Florida and Tempest Labs',
+        image: '',
+        twitter: 'dr_clint_gm',
+        linkedin: 'ClintonSnider',
+        order: 3,
+    },
+    {
+        name: 'Narbeh Shahnazarian',
+        title: 'Protocol Engineer',
+        description:
+            'Fullstack engineer and serial entrepreneur, passionate about building software for positive impact.',
+        experience: 'Expedia',
+        image: '',
+        twitter: 'narb_s',
+        linkedin: 'narbeh-shahnazarian-214a68a0',
+        order: 4,
+    },
+    {
+        name: 'Lindsay Walker',
+        title: 'Product Lead',
+        description:
+            'Driving product growth through technical expertise and strategic leadership.',
+        experience: 'Protocol Labs, Starling Lab Stanford & USC',
+        image: '',
+        twitter: '',
+        linkedin: 'LindsayJoWalker',
+        order: 5,
+    },
+    {
+        name: 'James Walker',
+        title: 'Head of Infrastructure',
+        description:
+            'Software developer advocating for a decentralized, open web.',
+        experience: 'Fission VP of Engineering',
+        image: '',
+        twitter: 'walkah',
+        linkedin: 'walkah',
+        order: 6,
+    },
+    {
+        name: 'Holly Grimm',
+        title: 'Senior Protocol and Blockchain Engineer',
+        description:
+            'Seasoned veteran in startups and Web3, blending enterprise software expertise with decentralized technology.',
+        experience: 'Gitcoin and Algovera',
+        image: '',
+        twitter: '',
+        linkedin: 'HollyGrimm',
+        order: 7,
+    },
+    {
+        name: 'Dr. Eric Boniardi',
+        title: 'Senior Research Engineer',
+        description:
+            'AI scholar and engineer bridging the gap between technical expertise and business acumen, shaping the future of Web3. PhD in ML Research.',
+        experience: 'Scholar - Mathematical Engineering',
+        image: '',
+        twitter: '',
+        linkedin: 'Eric-Boniardi',
+        order: 8,
+    },
+    {
+        name: 'Alex Mirran',
+        title: 'Growth & BD Lead',
+        description:
+            'Startup veteran and Web3 enthusiast, bridging enterprise software and decentralized technologies.',
+        experience: 'Willis Towers Watson, Disrupt, and Eze Software',
+        image: '',
+        twitter: 'Alex_Mirran',
+        linkedin: 'Alex-Mirran',
+        order: 9,
+    },
+    {
+        name: 'Nate Smith',
+        title: 'Partnerships and Business Development',
+        description:
+            'Forging strategic partnerships to drive business growth and ecosystem expansion.',
+        experience: 'GameStop and Godwoken',
+        image: '',
+        twitter: 'NatronSmith',
+        linkedin: 'NateSmith12',
+        order: 10,
+    },
+    {
+        name: 'Ayush Kumar',
+        title: 'Software Engineer',
+        description:
+            'Fullstack developer crafting user-centric applications for a decentralized future.',
+        experience: 'Protocol Labs, Moveable Ink, and Apybara Founder',
+        image: '',
+        twitter: '_Kelindi',
+        linkedin: 'Kelindi',
+        order: 11,
+    },
+    {
+        name: 'Arsen Yeremin',
+        title: 'Senior AI and Blockchain Solutions Engineer',
+        description:
+            'Fullstack developer with expertise in smart contracts, AI, and machine learning.',
+        experience: 'TypherAL CTO',
+        image: '',
+        twitter: 'Arsen3D',
+        linkedin: 'ArsenYeremin',
+        order: 12,
+    },
+    {
+        name: 'Brian Ginsburg',
+        title: 'Senior Software Engineer',
+        description:
+            'Web and decentralized systems expert, fueled by research papers, cycling trails, and Flamenco melodies.',
+        experience: 'Fission',
+        image: '',
+        twitter: '',
+        linkedin: 'Brian-Ginsburg',
+        order: 13,
+    },
+    {
+        name: 'Logan Lentz',
+        title: 'Software and Developer Relations Engineer',
+        description:
+            'Software engineer championing Web3 privacy and security, fostering a vibrant developer community.',
+        experience: 'AWS and Leto.gg Founder',
+        image: '',
+        twitter: 'ZaphodB17',
+        linkedin: 'Logan-Lentz',
+        order: 14,
+    },
+    {
+        name: 'Phil Billingsby',
+        title: 'Developer Relations and DX Engineer',
+        description:
+            'Advocate for developer empowerment, lowering the barrier to entry through education and tools.',
+        experience: 'Unboxed, Arweave, Roostify, and FilmFreeway',
+        image: '',
+        twitter: 'pbillingsby_',
+        linkedin: 'Phil-Billingsby-68a103303',
+        order: 15,
+    },
+    {
+        name: 'Taosheng Shi',
+        title: 'APAC Community Lead',
+        description:
+            'Blockchain enthusiast and experienced community builder, fostering collaboration and growth.',
+        experience: 'Filecoin and Nokia',
+        image: '',
+        twitter: 'TaoshengShi',
+        linkedin: 'TaoshengShi',
+        order: 16,
+    },
+    {
+        name: 'Sam Ceja',
+        title: 'Marketing, Events and Community Manager',
+        description:
+            'Experienced community builder cultivating engagement and positive web3 spaces. Passionate about education, gaming, and coding.',
+        experience: 'Godwoken and Developer DAO',
+        image: '',
+        twitter: 'iamsamcodes',
+        linkedin: 'iamsamcodes',
+        order: 17,
+    },
+    {
+        name: 'Devlin Rocha',
+        title: 'Developer Relations Engineer and Content Producer',
+        description:
+            'Polyglot senior software engineer crafting robust and scalable web applications. Inspired by music, gaming, and technology.',
+        experience: 'Synctree and Revolution Entertainment Services',
+        image: '',
+        twitter: 'DevlinRocha',
+        linkedin: 'DevlinRocha',
+        order: 18,
+    },
+]
+
+export const advisors = [
+    {
+        name: 'David Aronchick',
+        title: 'Distributed Compute and Product Advisor',
+        experience: 'Kubernetes, Bacalhau, and Expanso CEO',
+        image: '',
+        twitter: 'aronchick',
+        linkedin: 'aronchick',
+        order: 1,
+    },
+]
