f58bce71df5afa8f3099c89fa721b618425429a7
PBillingsby <philbillingsby@gmail.com>
2025-03-30 09:27:15 -0400

    update rewards for march

--- apps/rewards-dashboard/app/components/SocialLinks.js
+++ apps/rewards-dashboard/app/components/SocialLinks.js
@@ -13,50 +13,50 @@ const socialLinks = [
     },
 ]
 
-export default function SocialLinks({ currentView }) {
-    return (
-        <div className="container w-full py-12 md:flex md:justify-between md:py-4">
-            <div className="cta mb-8 text-center md:order-2 md:mb-0 md:text-left">
-                <div className="cta-text mb-4">
-                    <h3 className="cta-title text-xl font-bold">
-                        Get involved!
-                    </h3>
-                    {currentView === 'openSource' ? (
-                        <p className="cta-subtitle text-md">
-                            Join the Lilypad open source initiative now.
-                        </p>
-                    ) : (
-                        <p className="cta-subtitle text-md">
-                            Explore the Lilypad ambassador program now.
-                        </p>
-                    )}
-                </div>
-                <a
-                    href={
-                        currentView === 'openSource'
-                            ? 'https://blog.lilypadnetwork.org/lilypad-launches-open-source-initiative-as-part-of-incentivenet'
-                            : 'https://lilypadnetwork.notion.site/Lilypad-Ambassadors-f11f73e91f684fa192fc2fab7985fe0d'
-                    }
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="cta-button rounded-md bg-[#095856] px-4 py-2 text-[#E0FFF9] hover:bg-[#0c7472]"
-                >
-                    Find out more!
-                </a>
-            </div>
+export default function SocialLinks({currentView}) {
+	const ctaConfig = {
+		openSource: {
+		  title: "Get involved!",
+		  description: "Join the Lilypad open source initiative now.",
+		  link: "https://blog.lilypadnetwork.org/lilypad-launches-open-source-initiative-as-part-of-incentivenet",
+		},
+		ambassador: {
+		  title: "Get involved!",
+		  description: "Explore the Lilypad ambassador program now.",
+		  link: "https://lilypadnetwork.notion.site/Lilypad-Ambassadors-f11f73e91f684fa192fc2fab7985fe0d",
+		},
+		modules: {
+		  title: "Create a Lilypad module!",
+		  description: "Package your AI model to run on Lilypad",
+		  link: "https://docs.lilypad.tech/lilypad/developer-resources/lilypad-modules",
+		},
+	  };
+	  const currentCTA = ctaConfig[currentView];
 
-            <div className="order-1 flex items-center justify-center gap-4 md:order-1 md:justify-start">
-                {socialLinks.map((link, index) => (
-                    <a
-                        key={index}
-                        href={link.href}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <img src={link.iconUrl} alt="social icon" />
-                    </a>
-                ))}
-            </div>
-        </div>
-    )
+  return (
+    <div className="md:flex md:justify-between container w-full py-12 md:py-4">
+      <div className="cta md:order-2 text-center md:text-left mb-8 md:mb-0">
+		<div className="cta-text mb-4">
+			<h3 className="cta-title text-xl font-bold">{currentCTA.title}</h3>
+			<p className="cta-subtitle text-md">{currentCTA.description}</p>
+		</div>
+		<a
+			href={currentCTA.link}
+			target="_blank"
+			rel="noopener noreferrer"
+			className="cta-button bg-[#095856] text-[#E0FFF9] px-4 py-2 rounded-md hover:bg-[#0c7472]"
+		>
+			Find out more!
+		</a>
+		</div>
+
+      <div className="flex order-1 md:order-1 gap-4 justify-center md:justify-start items-center">
+        {socialLinks.map((link, index) => (
+          <a key={index} href={link.href} target="_blank" rel="noopener noreferrer">
+            <img src={link.iconUrl} alt="social icon" />
+          </a>
+        ))}
+      </div>
+    </div>
+  );
 }
